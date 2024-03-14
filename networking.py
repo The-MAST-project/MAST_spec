b4ce07f949dfdb50fad137e14f570d83d22b8ce0
Arie Blumenzweig <theblumz@gmail.com>
2024-03-14 17:59:29 +0200

    Added NetworkedDevice with its own configuration

---
+++ networking.py
@@ -0,0 +1,73 @@
+from config.config import Config, deep_search, DeepSearchResult
+import pythonping
+import logging
+from utils import init_log
+from typing import Tuple, List
+
+logger = logging.getLogger('networking')
+init_log(logger, logging.DEBUG)
+
+
+class NetworkDestination:
+
+    def __init__(self, address: str, port: int):
+        self.address: str = address
+        self.port: int = port
+
+
+class NetworkedDevice:
+    """
+    A device accessed via an IP connection
+    """
+
+    def __init__(self, conf: dict):
+        """
+
+        :param conf: A dictionary with keys:
+            - 'address' - [Mandatory] The IP address of the device
+            - 'port'    - [Optional] Port
+        """
+
+        if 'network' not in conf:
+            raise Exception(f"Missing 'network' key in {conf}")
+        if 'network' not in conf:
+            raise Exception(f"Missing 'network.address' key in {conf}")
+
+        self.address = conf['network']['address']
+        self.port = int(conf['network']['port']) if 'port' in conf['network'] else None
+
+        self.destination = NetworkDestination(address=self.address, port=self.port)
+
+
+def ping_peers(verbose: bool = False) -> Tuple[List, List]:
+    """
+    ICMP pings all the configured peers
+    :param verbose: If True, output results
+    :return: Tuple(list of successes, list of failures)
+    """
+    conf = Config().toml
+    failed = []
+    succeeded = []
+
+    responses = deep_search(conf, 'address')
+    for response in responses:
+        peer_name = response.path
+        peer_name = peer_name.removesuffix('.network.address')
+
+        response = pythonping.ping(response.value, timeout=2, count=1)
+        if response.stats_success_ratio == 1.0:
+            succeeded.append(peer_name)
+            if verbose:
+                logger.info(f"{peer_name} responds to ping")
+        else:
+            failed.append(peer_name)
+            if verbose:
+                logger.error(f"{peer_name} does not respond to ping")
+
+    return succeeded, failed
+
+
+if __name__ == '__main__':
+    successes, failures = ping_peers(verbose=False)
+    print(f"responding:     {successes}")
+    print(f"not responding: {failures}")
