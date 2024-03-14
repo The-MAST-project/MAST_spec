a978bf68e078d864534dc530cd5218b37e862ba1
Arie Blumenzweig <theblumz@gmail.com>
2024-03-14 18:05:58 +0200

    added deep_search()

--- config/config.py
+++ config/config.py
@@ -1,5 +1,6 @@
 import tomlkit
 import os
+from typing import List
 
 
 class Config:
@@ -25,3 +26,40 @@ class Config:
         self.toml.clear()
         with open(self.file, 'r') as f:
             self.toml = tomlkit.load(f)
+
+
+class DeepSearchResult:
+
+    def __init__(self, path: str, value):
+        self.path = path
+        self.value = value
+
+
+def deep_search(d:dict, what:str, path: str = None, found: list = None) -> List[DeepSearchResult]:
+    """
+    Performs a deep search of a keyword in a dictionary
+    :param d: The dictionary to be searched
+    :param what: The keyword to search for
+    :param path:
+    :param found:
+    :return:
+    """
+
+    if found is None:
+        found = list()
+
+    for key, value in d.items():
+        if isinstance(d[key], dict):
+            deep_search(d[key], what, key if path is None else path + '.' + key, found)
+        else:
+            if key == what:
+                f = DeepSearchResult(key if path is None else path + '.' + key, value)
+                found.append(f)
+                return found
+    return found
+
+
+if __name__ == '__main__':
+    results = deep_search(Config().toml, 'address')
+    for result in results:
+        print(f"{result.path=}, {result.value=}")
