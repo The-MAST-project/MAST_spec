9318fc685923cbe1da455372edc88f111eccf29a
Arie Blumenzweig <theblumz@gmail.com>
2024-03-14 18:02:18 +0200

    WIP

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import uvicorn
 from fastapi import FastAPI
 from config.config import Config
+from networking import ping_peers
 
 from stage.stage import router as stage_router
 from filter_wheel.wheel import router as filter_wheel_router
+from cameras.andor.newton import router as highspec_camera_router
 
 app = FastAPI()
 
+responding, not_responding = ping_peers()
+print(f"responding peers:     {responding}")
+print(f"not-responding peers: {not_responding}")
+
 app.include_router(stage_router)
 app.include_router(filter_wheel_router)
+app.include_router(highspec_camera_router)
 
 
 if __name__ == '__main__':
