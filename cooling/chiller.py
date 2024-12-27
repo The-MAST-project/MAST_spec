6d78cc983485a7ad61dd228689851247ae8258cf
Arie Blumenzweig <theblumz@gmail.com>
2024-12-27 13:30:23 +0200

    cleanups + fastapi

--- cooling/chiller.py
+++ cooling/chiller.py
@@ -1,8 +1,9 @@
 from typing import List
 
 from common.dlipowerswitch import SwitchedOutlet, OutletDomain
-from common.utils import Component
+from common.utils import Component, BASE_SPEC_PATH
 from common.config import Config
+from fastapi.routing import APIRouter
 
 
 class Chiller(SwitchedOutlet, Component):
@@ -11,7 +12,7 @@ class Chiller(SwitchedOutlet, Component):
         self.conf = Config().get_specs()['chiller']
         SwitchedOutlet.__init__(self, domain=OutletDomain.Spec, outlet_name='Chiller')
 
-        if not self.switch.detected:
+        if not self.power_switch.detected:
             return
 
         if not self.is_on():
@@ -54,15 +55,15 @@ class Chiller(SwitchedOutlet, Component):
 
     @property
     def operational(self) -> bool:
-        return self.switch.detected and self.is_on()
+        return self.power_switch.detected and self.is_on()
 
     @property
     def why_not_operational(self) -> List[str]:
         ret = []
-        if not self.switch.detected:
-            ret.append(f"chiller: power switch '{self.switch.name}' (at '{self.switch.ipaddress}') not detected")
-        if not self.is_on():
-            ret.append('chiller: not powered on')
+        if not self.power_switch.detected:
+            ret.append(f"chiller: {self.power_switch} not detected")
+        elif self.is_off():
+            ret.append('chiller: {self.power_switch}:{self.outlet_name} is OFF')
         return ret
 
     def status(self):
@@ -70,3 +71,11 @@ class Chiller(SwitchedOutlet, Component):
             'operational': self.operational,
             'why_not_operational': self.why_not_operational,
         }
+
+chiller = Chiller()
+
+base_path = BASE_SPEC_PATH + 'chiller'
+tag = 'Chiller'
+router = APIRouter()
+
+router.add_api_route(base_path + '/status', tags=[tag], endpoint=chiller.status)
