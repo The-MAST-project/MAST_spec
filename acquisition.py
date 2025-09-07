b032b2cb7087037b17bbad05cd672beb55bf8899
Arie Blumenzweig <theblumz@gmail.com>
2025-09-07 10:51:44 +0300

    formatting

--- acquisition.py
+++ acquisition.py
@@ -1,9 +1,9 @@
-from utils import PathMaker
 import uuid
+
+from common.utils import PathMaker
 
 
 class Acquisition:
-
     def __init__(self):
         self.uuid = str(uuid.uuid1())
-        self.dir = PathMaker().make_acquisition_folder_name(acquisition=self.uuid)
+        self.dir = PathMaker().make_acquisition_folder()
